746c7661e9186c923c68af54ac6b92425aa8559a
Cano XL <gcanoxl@gmail.com>
2023-11-30 13:10:05 +0800

    feat: add `add_const`

--- lua/dart-const-organizer/init.lua
+++ lua/dart-const-organizer/init.lua
@@ -4,16 +4,16 @@ local ts_utils = require("nvim-treesitter.ts_utils")
 
 local function find_const_node()
 	local node = ts_utils.get_node_at_cursor()
-	local search_path = node:type()
+	-- local search_path = node:type()
 	while node:type() ~= 'const_object_expression' and node:type() ~= 'program' do
 		node = node:parent()
-		search_path = search_path .. ' -> ' .. node:type()
+		-- search_path = search_path .. ' -> ' .. node:type()
 	end
-	print(search_path)
+	-- print(search_path)
 	return node:child(0)
 end
 
-local function remove_const(err)
+local function remove_const(_)
 	local node = find_const_node()
 	local lnum, start_col, _, end_col = node:range()
 	local line = vim.api.nvim_buf_get_lines(0, lnum, lnum + 1, false)[1]
@@ -21,14 +21,25 @@ local function remove_const(err)
 	vim.api.nvim_buf_set_lines(0, lnum, lnum + 1, false, { new_line })
 end
 
+local function add_const(err)
+	print(err)
+	vim.lsp.buf.code_action({
+		apply = true,
+		filter = function(action)
+			return action.title == "Add 'const' modifier"
+		end
+	})
+end
+
 
 local function callback(args)
-	local errors = vim.diagnostic.get(args.buf, { severity = vim.diagnostic.severity.ERROR })
+	local errors = vim.diagnostic.get(args.buf)
 	for _, err in ipairs(errors) do
-		print(err.code)
 		if err.code == 'const_with_non_const' or
 				err.code == 'const_eval_method_invocation' then
 			remove_const(err)
+		elseif err.code == 'prefer_const_constructors' then
+			add_const(err)
 		end
 	end
 end
